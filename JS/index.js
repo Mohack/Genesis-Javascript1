db85e82a638a17d86a194c4ef648a54455c38fb0
Mohak <mohakchourasia@gmail.com>
2022-01-09 16:43:03 +0530

    Initial commit

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+let tagName = document.getElementsByTagName('h1');
+let className = document.getElementsByClassName('container');
+let idName = document.getElementById('title');
+let querySelector = document.querySelector('h1');
+let querySelectorAll = document.querySelectorAll
+
+console.log(tagName);
+console.log(className);
+console.log(idName);
+console.log(querySelector);
