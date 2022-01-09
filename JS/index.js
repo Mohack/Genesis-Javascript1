0f6c88d8898a5383b7cb4f641de9ab7d73cfccef
Mohack <69666452+Mohack@users.noreply.github.com>
2022-01-09 19:00:09 +0530

    1st Changes

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,19 @@
-let tagName = document.getElementsByTagName('h1');
+let tagName = document.getElementsByTagName('li');
 let className = document.getElementsByClassName('container');
 let idName = document.getElementById('title');
 let querySelector = document.querySelector('h1');
-let querySelectorAll = document.querySelectorAll
+let querySelectorAll = document.querySelectorAll('li');
 
 console.log(tagName);
 console.log(className);
 console.log(idName);
 console.log(querySelector);
+console.log(querySelectorAll);
+console.log(document.querySelectorAll('.todo-list-element'));
+console.log(document.querySelectorAll('#add'));
+
+console.log(document.querySelector('h1').getAttribute('class'));
+document.querySelector('h1').setAttribute('class','todo-list-element');
+document.querySelector('h1').style.backgroundColor = "red";
+document.querySelector('h1').className='myStyle';
+document.querySelector('li').classList;
